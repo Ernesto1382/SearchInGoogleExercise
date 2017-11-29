83ffef1b67a7cc97e3931ee50bd0d83a1b31d518
Ernesto1382 <34061879+Ernesto1382@users.noreply.github.com>
2017-11-29 01:37:17 -0300

    Add files via upload

--- src/intive_fdv_GoogleExercise.java
+++ src/intive_fdv_GoogleExercise.java
@@ -10,10 +10,12 @@ public class intive_fdv_GoogleExercise {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		//if you didn't update the Path system variable to add the full directory path to the executable as above mentioned then doing this directly through code
 		
+		//Use for Firefox
 		// Create a new instance of the Firefox driver
 				//WebDriver driver = new FirefoxDriver();
+		
+		//Use for Chrome
 		// Create a new instance of the Chrome driver
 				System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 				WebDriver driver = new ChromeDriver();
@@ -33,26 +35,28 @@ public class intive_fdv_GoogleExercise {
 		         * the result as "Passed" or "Failed"
 		         */
 		        if (actualTitle.contentEquals(expectedTitle)){
-		            //WebElement Searchbox = driver.findElement(By.id("lst-ib"));
+		            WebElement Searchbox = driver.findElement(By.id("lst-ib"));
 		            //Other ways to look the searchbox
 		            //WebElement Searchbox = driver.findElement(By.name("q"));
 		            //WebElement Searchbox = driver.findElement(By.className("gsfi"));
 		            //WebElement Searchbox = driver.findElement(By.xpath("//input[@id='lst-ib']"));
-		            WebElement Searchbox = driver.findElement(By.xpath("//input[@title='Search']"));
+		            
 		            Searchbox.sendKeys(WordSearched);
-		            //Searchbox.submit();
-		            //driver.findElement(By.name("btnK")).click();
+		            
+		            //The Test Case request use ENTER, but other way is Searchbox.submit();
 		            driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		            
+		            //identify the link by the title of shows in the search
 		            driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+		            
+		            //This line helpme to have a follow in the console
 		            System.out.println("Test Passed!");
 		        } else {
 		            System.out.println("Test Failed");
 		        }
 
-		        // Print a Log In message to the screen
-		        //System.out.println("Successfully opened the website www.Store.Demoqa.com");
 
-				//Wait for 5 Sec
+				//Wait for 1 Sec
 				Thread.sleep(1000);
 				
 		        // Close the driver
